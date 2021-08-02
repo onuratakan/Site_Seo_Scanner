da823ec7c9555f562bd8208b199e0145cfefb0ee
Onur Atakan ULUSOY <atadogan06@gmail.com>
2021-08-02 16:42:59 +0000

    badge fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Site Seo Scanner | ![Made_with_python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg) ![Licence](https://img.shields.io/github/license/onuratakan/Site_Seo_Scanner.svg)
+# Site Seo Scanner | ![Made_with_python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg) ![Licence](https://img.shields.io/github/license/onuratakan/Site_Seo_Scanner)
 # Installing
 ```console
 pip install Site-Seo-Scanner
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='Site_Seo_Scanner',
-version='0.1.0',
+version='0.1.1',
 description="""The site seo scanner.
 """,
 long_description="""
-# Site Seo Scanner | ![Made_with_python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg) ![Licence](https://img.shields.io/github/license/onuratakan/Site_Seo_Scanner.svg)
+# Site Seo Scanner | ![Made_with_python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg) ![Licence](https://img.shields.io/github/license/onuratakan/Site_Seo_Scanner)
 # Installing
 ```console
 pip install Site-Seo-Scanner
